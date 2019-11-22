64f7a2094ab958754462428f71529d550353d8ba
luisdiogo98 <luisdiogo98@users.noreply.github.com>
2019-11-22 10:25:00 +0000

    Add basic user sign up

--- models/employeeModel.js
+++ models/employeeModel.js
@@ -1,7 +1,25 @@
+const utils = require('../utils');
+const { pool } = require('../config');
+
 const getEmployees = () => {
     return {
         "ola": "bom dia"
     };
 }
 
-module.exports = { getEmployees }
+const signup = async (email, password) => {
+    const createQuery = `INSERT INTO
+      employee(email, password)
+      VALUES($1, $2)
+      returning *`;
+    const values = [
+        email,
+        password
+    ];
+
+    const { rows } = await pool.query(createQuery, values);
+    const token = utils.generateToken(rows[0].id);
+    return token;
+}
+
+module.exports = { getEmployees, signup }
