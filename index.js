e494ceae234fb09b36f21610506f36af90f49c66
luisdiogo98 <luisdiogo98@users.noreply.github.com>
2019-11-22 09:12:09 +0000

    Fix api error view

--- index.js
+++ index.js
@@ -13,10 +13,6 @@ var logger = require('morgan');
 
 const app = express();
 
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
-
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
@@ -62,7 +58,7 @@ app.use(function(err, req, res, next) {
 
   // render the error page
   res.status(err.status || 500);
-  res.render('error');
+  res.json({ error: err });
 });
 
 // Start server
