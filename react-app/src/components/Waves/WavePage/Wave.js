0f3378298ee54fd0d8cc3eb7d477ad32ad94c5e8
Francisco Filipe <francilipe.7@gmail.com>
2019-12-07 18:14:36 +0000

    Fix wave page

--- react-app/src/components/Waves/WavePage/Wave.js
+++ react-app/src/components/Waves/WavePage/Wave.js
@@ -5,15 +5,12 @@ import scrollStyles from "../../../styles/scroll.module.css";
 import classNames from "classnames";
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
-import Container from "react-bootstrap/Container";
-import Button from "react-bootstrap/Button";
-import { FaUserAlt } from 'react-icons/fa';
 import defaultImg from "../../../assets/profile_pics/default.png";
 
 const Wave = ({ wave }) => {
   const inventoryIncoming = [
     { id: 1, name: "Black Air Forces", quantity: 10, order: '01' },
-    { id: 2, name: "Supreme Shovel", quantity: 69, order: '13'},
+    { id: 2, name: "Supreme Shovel", quantity: 69, order: '13' },
     { id: 3, name: "Golos do Abou", quantity: 2, order: '07' },
     { id: 4, name: "Pernas do André Gomes", quantity: 1, order: '02' },
     { id: 5, name: "Champions do Benfica", quantity: 0, order: '08' }
@@ -23,52 +20,48 @@ const Wave = ({ wave }) => {
   return (
     <div className={wavesStyles.wavesInfoContainer}>
       <div className={wavesStyles.size}>
-      <Row className={wavesStyles.separator}>
-        <h3>Wave - {wave.id}</h3>
-      </Row>
-      <Row>
         <Row className={wavesStyles.separator}>
-          <h4 className={wavesStyles.infoTitles}>Employee Assigned</h4>
+          <h3>Wave - {wave.id}</h3>
         </Row>
-        <Row >
-          <Col className={wavesStyles.nopadding} md="1">
+        <Row className={classNames(wavesStyles.separator, wavesStyles.center)}>
+          <Col className={wavesStyles.nopadding} md="2">
             <img
               className={wavesStyles.userImg}
               src={defaultImg}
               alt="product"
             />
           </Col>
-          <Col className={wavesStyles.inline} >
-            In Young Jang
+          <Col >
+            <h4 className={wavesStyles.infoTitles}>Employee Assigned</h4>
+            <p>{wave.emp_name}</p>
           </Col>
-        </Row>       
-      </Row>
-          <Row className={wavesStyles.title}>
-            <h4>Product List</h4>
-          </Row>
-          <Row className={wavesStyles.itemheader}>
-            <Col md="10" style={{display:"flex"}}>
-              <Col md="2" style={{marginLeft: '0.5em'}} >
-                <h5>ID</h5>
-              </Col>
-              <Col md="5">
-                <h5>Product Name</h5>
-              </Col>
-              <Col md="3" style={{marginLeft: '0.2em'}}>
-                <h5>Quantity</h5>
-              </Col>
-              <Col md="1" style={{marginLeft: '0.5em'}}>
-                <h5>Order</h5>
-              </Col>
+        </Row>
+        <Row className={classNames(wavesStyles.title, wavesStyles.padding_top)}>
+          <h4>Product List</h4>
+        </Row>
+        <Row className={wavesStyles.itemheader}>
+          <Col md="10" style={{ display: "flex" }}>
+            <Col md="2" style={{ marginLeft: '0.5em' }} >
+              <h5>ID</h5>
             </Col>
-          </Row>
-          <div className={classNames(scrollStyles.scroll,wavesStyles.scroll30)}>
-            {inventoryIncoming.map(item => (
-              <ItemOrder item={item} />
-            ))}{" "}
-          </div>
-      
+            <Col md="5">
+              <h5>Product Name</h5>
+            </Col>
+            <Col md="3" style={{ marginLeft: '0.2em' }}>
+              <h5>Quantity</h5>
+            </Col>
+            <Col md="1" style={{ marginLeft: '0.5em' }}>
+              <h5>Order</h5>
+            </Col>
+          </Col>
+        </Row>
+        <div className={classNames(scrollStyles.scroll, wavesStyles.scroll30)}>
+          {inventoryIncoming.map(item => (
+            <ItemOrder item={item} />
+          ))}{" "}
         </div>
+
+      </div>
     </div>
   );
 };
