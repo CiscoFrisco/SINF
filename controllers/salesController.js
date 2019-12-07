23f6d0ef6f1b9cc021f218ce912f5a2e13510ecf
Rekicho <up201604145@fe.up.pt>
2019-12-07 16:30:57 +0000

    Sales take into account deliveries

--- controllers/salesController.js
+++ controllers/salesController.js
@@ -1,45 +1,58 @@
 const request = require("request");
+let incomingDeliveries = [];
 
-const getSales = (req, res, next) => {
+const getDeliveries = (req, res, next) => {
     var options = {
-        headers: {
-            Authorization: process.env.PRIMAVERA_TOKEN,
-            "Content-Type": "application/json",
-        },
         method: 'GET',
-        url: `https://${process.env.PRIMAVERA_URL}/api/${process.env.PRIMAVERA_TENANT}/${process.env.PRIMAVERA_ORGANIZATION}/sales/orders`,
+        url: `${req.protocol}://${req.get('host')}/api/stock/delivery`,
     };
 
     request(options, (error, response, body) => {
-        if(error) {
-            res.status(response.statusCode).send(error);
-        }
+        incomingDeliveries = JSON.parse(body);
+        options = {
+            headers: {
+                Authorization: process.env.PRIMAVERA_TOKEN,
+                "Content-Type": "application/json",
+            },
+            method: 'GET',
+            url: `https://${process.env.PRIMAVERA_URL}/api/${process.env.PRIMAVERA_TENANT}/${process.env.PRIMAVERA_ORGANIZATION}/sales/orders`,
+        };
 
-        const orders = [];
-
-        JSON.parse(body).forEach((element) => {
-            if(!element.isDeleted && !element.naturalKey.includes("SYS")) {
-                const order = {
-                    id: element.naturalKey,
-                    name: element.buyerCustomerPartyName,
-                    date: element.documentLines[0].deliveryDate,
-                    productList: [],
-                };
-
-                element.documentLines.forEach((line) => {
-                    order.productList.push({
-                        id: line.salesItem,
-                        name: line.description,
-                        quantity: line.quantity,
-                    });
-                })
-
-                orders.push(order);
+        request(options, (error, response, body) => {
+            if (error) {
+                res.status(response.statusCode).send(error);
             }
+
+            const orders = [];
+
+            JSON.parse(body).forEach((element) => {
+                if (!element.isDeleted && !element.naturalKey.includes("SYS") && incomingDeliveries.includes(element.naturalKey)) {
+                    const order = {
+                        id: element.naturalKey,
+                        name: element.buyerCustomerPartyName,
+                        date: element.documentLines[0].deliveryDate,
+                        productList: [],
+                    };
+
+                    element.documentLines.forEach((line) => {
+                        order.productList.push({
+                            id: line.salesItem,
+                            name: line.description,
+                            quantity: line.quantity,
+                        });
+                    })
+
+                    orders.push(order);
+                }
+            });
+
+            res.status(response.statusCode).send(JSON.stringify(orders));
         });
-        
-        res.status(response.statusCode).send(JSON.stringify(orders));
     });
+}
+
+const getSales = (req, res, next) => {
+    getDeliveries(req, res, next);
 }
 
 module.exports = { getSales }
--- controllers/stockController.js
+++ controllers/stockController.js
@@ -27,4 +27,27 @@ const getIncoming = (req, res, next) => {
     });
 }
 
-module.exports = { getIncoming }
+const getDelivery = (req, res, next) => {
+    var options = {
+        headers: {
+            Authorization: process.env.PRIMAVERA_TOKEN,
+            "Content-Type": "application/json",
+        },
+        method: 'GET',
+        url: `https://${process.env.PRIMAVERA_URL}/api/${process.env.PRIMAVERA_TENANT}/${process.env.PRIMAVERA_ORGANIZATION}/shipping/processOrders/${pageIndex}/${pageSize}?company=${company}`,
+    };
+
+    request(options, (error, response, body) => {
+        if(error) {
+            res.status(response.statusCode).send(error);
+        }
+
+        let incomingDeliveries = [];
+
+        JSON.parse(body).forEach((element) => incomingDeliveries.push(element.sourceDocKey));
+        
+        res.status(response.statusCode).send(JSON.stringify(incomingDeliveries));
+    });
+}
+
+module.exports = { getIncoming, getDelivery }
