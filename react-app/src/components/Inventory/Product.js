0e2c25e148f489bba45b2230c7a450303d1717bd
luisdiogo98 <luisdiogo98@users.noreply.github.com>
2019-12-13 10:13:10 +0000

    Add request and order to inventory

--- react-app/src/components/Inventory/Product.js
+++ react-app/src/components/Inventory/Product.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import Row from 'react-bootstrap/Row'
 import Col from 'react-bootstrap/Col'
 import productStyles from '../../styles/inventory.module.css';
@@ -11,11 +11,46 @@ import scrollStyles from '../../styles/scroll.module.css'
 
 const Product = ({ product }) => {
 
-    const requests = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14];
-    const orders = [11, 12, 13, 14, 15, 16, 17, 18];
+    const [isLoadingOne, setIsLoadingOne] = useState(true);
+    const [isLoadingTwo, setIsLoadingTwo] = useState(true);
+    const [requests, setRequests] = useState([]);
+    const [orders, setOrders] = useState([]);
+
     const locations = [1, 5, 17, 8,1,1,1,1,1];
 
-    return (
+    useEffect(() => {
+        fetch("/api/purchases/requests", {
+            method: 'GET',
+            headers: {
+                'Accept': 'application/json',
+                'Content-Type': 'application/json',
+            },
+        })
+        .then(response => response.json())
+        .then(data => {
+            setRequests(data.filter(request => request.productList.some((x) => x.id == product.id)).map(request => request.id));
+            setIsLoadingOne(false);
+        })
+        .catch(console.log);
+    },[]);
+
+    useEffect(() => {
+        fetch("/api/sales/orders", {
+            method: 'GET',
+            headers: {
+                'Accept': 'application/json',
+                'Content-Type': 'application/json',
+            },
+        })
+        .then(response => response.json())
+        .then(data => {
+            setOrders(data.filter(order => order.productList.some((x) => x.id == product.id)).map(order => order.id));
+            setIsLoadingTwo(false);
+        })
+        .catch(console.log);
+    },[]);
+
+    return ((isLoadingOne || isLoadingTwo) ? <div><p>Loading...</p></div> : (
         <div className={productStyles.productInfoContainer}>
             <Row className={classNames(productStyles.separator, productStyles.inline)}>
                 <h3>{product.id} - {product.name}</h3>
@@ -43,7 +78,7 @@ const Product = ({ product }) => {
                 </Col>
             </Row>
         </div >
-    )
+    ))
 }
 
 export default Product;
